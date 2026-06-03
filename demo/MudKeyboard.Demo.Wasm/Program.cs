2e6b1bd7cc88d20128f5116d7638d514f959f217
sardar <ssardarqaslany@yahoo.com>
2026-06-04 00:17:54 +0100

    complete wasm demo

--- demo/MudKeyboard.Demo.Wasm/Program.cs
+++ demo/MudKeyboard.Demo.Wasm/Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using MudKeyboard.Demo.Wasm;
+using MudKeyboard.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices();
+
+// Registers the global docked-keyboard services. Pair with a single <MudKeyboardHost /> in the layout.
+builder.Services.AddMudKeyboard();
 
 await builder.Build().RunAsync();
